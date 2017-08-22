a1681b1981bd42b22ee24d3102b95658b6285896
Carlos Pano H <carlospano8@gmail.com>
2017-08-22 09:41:54 -0500

    Primer programa.

--- Hipotenusa.py
+++ Hipotenusa.py
@@ -2,3 +2,12 @@
 #Descripcion: Escribe un programa que lee la longitud de los catetos de un triangulo rectangulo e imprime la longitud de la hipotenusa.
 
 #Paso 1:
+catetoA = float(input("Introduce la longitud de tu cateto A(cm):"))
+catetoB = float(input("Introduce la longitud de tu cateto B(cm):"))
+
+#Paso 2:
+hipotenusa = (catetoA**2+catetoB**2)**0.5
+
+#Paso 3:
+print("La hipotenusa de tu Triangulo es igual a:", hipotenusa)
+
