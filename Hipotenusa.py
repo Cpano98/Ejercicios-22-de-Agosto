dc5f7d79fd8fe5c111fc7bdbd80a4d1696f38eb5
Carlos Pano H <carlospano8@gmail.com>
2017-08-22 09:34:17 -0500

    Primer programa.

---
+++ Hipotenusa.py
@@ -0,0 +1,4 @@
+#Autor: Carlos Pano Hernandez - A01066264
+#Descripcion: Escribe un programa que lee la longitud de los catetos de un triangulo rectangulo e imprime la longitud de la hipotenusa.
+
+#Paso 1:
